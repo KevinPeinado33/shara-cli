c941d8417164f5062737580d0b8d7f1fb16d9e32
KevinPeinado33 <kevinpeinado@upeu.edu.pe>
2019-12-07 11:44:48 -0500

    iniciando el login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-import NavBar from './layouts/navbar/NavBar';
-import SideBar from './layouts/sidebar/SideBar';
+import Login from './components/login/Login';
 
 function App() {
   return (
     <div className="wrapper">
-      <NavBar />
-      <SideBar />
+      <Login />
     </div>
   );
 }
